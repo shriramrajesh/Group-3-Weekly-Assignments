9ca239b62b6073ddfd7cd2f2aa8247188f1adc61
Shriram <90387381+srr910@users.noreply.github.com>
2021-09-12 11:40:43 +0530

    Update Week 1- Rock Paper Scissors Game (Group).py
    Problem Statement
    
    Make a two-player Rock-Paper-Scissors game. One of the players is the computer.
    
    10 rounds. Print out the winner and points earned by both players at the end of the game.
    
    Remember the rules:
    
    Rock beats Scissors
    Scissors beats Paper
    Paper beats Rock
    
    
    Use a dictionary to store the history of choices made by the Player and the Computer. 
    
    
    You should be able to print the choices made by the Player and the Computer in each round once the game is completed.
    
    
    Example - 
    
    
    
    Enter the round for which you need the information >> 3
    
    
    Output
    
    
    Player choice = Rock
    
    Computer choice = Paper
    
    Player won Round 3

--- Week 1- Rock Paper Scissors Game (Group).py
+++ Week 1- Rock Paper Scissors Game (Group).py
@@ -1,36 +1,36 @@
 from random import randint
-c={'rock':0,'paper':1,'scissors':2 }
-d1= ['rock','paper','scissors']
-d2={}
-q=0
-t=0
-d='y'
+c = {'rock': 0, 'paper': 1, 'scissors': 2}
+d1 = ['rock', 'paper', 'scissors']
+d2 = {}
+q = 0
+t = 0
+d = 'y'
 for i in range(1,11):
-    p=input("Enter player's choice")
-    u = d1[randint(0,2)]
-    print("Computer's choice=%s" %(u))
-    outcomes = {0: {0: "Tie", 1: "Computer won", 2: "Player won"},1: {0: "Player won", 1: "Tie", 2: "Computer won"},2: {0: "Computer won", 1: "Player won", 2: "Tie"}}
-    if((c[p]==0 and c[u]==2) or (c[p]==1 and c[u]==0) or (c[p]==2 and c[u]==1)):
-        q=q+1
+    p = input("Enter player's choice")
+    u = d1[randint(0, 2)]
+    print("Computer's choice=%s" % (u))
+    outcomes = {0: {0: "Tie", 1: "Computer won", 2: "Player won"}, 1: {0: "Player won", 1: "Tie", 2: "Computer won"},2: {0: "Computer won", 1: "Player won", 2: "Tie"}}
+    if ((c[p] == 0 and c[u] == 2) or (c[p] == 1 and c[u] == 0) or (c[p] == 2 and c[u] == 1)):
+        q = q + 1
     else:
         if ((c[p] == 0 and c[u] == 1) or (c[p] == 1 and c[u] == 2) or (c[p] == 2 and c[u] == 0)):
-            t=t+1
+            t = t + 1
     result = outcomes[c[p]][c[u]]
-    d2[i] = [p,u,result]
+    d2[i] = [p, u, result]
     print(result)
 print("Player's points= %d and Computer's points= %d" % (q, t))
-if(q>t):
-    print("Player won")
-elif(q<t):
-    print("Computer Won")
+if (q > t):
+    print("Player is the winner")
+elif (q < t):
+    print("Computer is the winner")
 else:
-    print("Tie")
+    print("It is a tie")
 while (d == 'y'):
     try:
         x = int(input("Enter the round for which you need the information >>"))
-        print("Player's's choice=%s" %(d2[x][0]))
-        print("Computer's choice=%s" %(d2[x][1]))
-        print(d2[x][2])
+        print("Player's's choice=%s" % (d2[x][0]))
+        print("Computer's choice=%s" % (d2[x][1]))
+        print(d2[x][2]+" round %d" %(x))
     except KeyError:
         print("Value should be of given range (1-10)")
     except ValueError:
