09129ca78515fb049cd56ab0d5fdeb4d11b3a356
Shriram <90387381+shriramrajesh@users.noreply.github.com>
2021-09-20 18:20:05 +0530

    Update Week 2- (Individual) Tic Tac Toe Game.py
    Problem Statement
    Create a Tic Tac Toe game.
    
    Here are the requirements:
    
    Two players should be able to play the game (user, computer)
    The board should be printed out every time a player makes a move
    You should be able to accept input of the player position and then place a symbol on the board
    - Store the Game logs (info about the moves, result) for 10 games into a data structure.
    - You should be able to recreate the moves and print out the board for each of the moves for any of the 10 games when the user inputs the game number.
    For example - if the user says that he wants to see how Game 3 was played, you should print out all the moves on the board and print out the winner at the end of the game.

--- Week 2- (Individual) Tic Tac Toe Game.py
+++ Week 2- (Individual) Tic Tac Toe Game.py
@@ -1,65 +1,76 @@
+import random
+import numpy as np
+
 def display_board(board):
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
+    print('   |   | \n' ' ' + board[0][0] + ' | ' + board[0][1] + ' | ' + board[0][2])
+    print('   |   | \n -----------',
+          '\n   |   | \n ' + board[1][0] + ' | ' + board[1][1] + ' | ' + board[1][2],
+          '\n   |   | \n ----------- \n',
+          ' ' ' |   | \n' + board[2][0] + '  | ' + board[2][1] + ' | ' + board[2][2],
+          '\n   |   |  ')
+
 def player_input():
     marker = ''
     while not (marker == 'X' or marker == 'O'):
         marker = input('Player : Do you want to be X or O? ').upper()
     if marker == 'X':
-        return ('X', 'O')
+        return 'X', 'O'
     else:
-        return ('O', 'X')
+        return 'O', 'X'
+
 def place_marker(board, marker, position):
-    board[position] = marker
+    board[int(position[0])][int(position[1])] = marker
+
 def win_check(board, mark):
-    return ((board[1] == mark and board[2] == mark and board[3] == mark) or  # across the top
-            (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
-            (board[7] == mark and board[8] == mark and board[9] == mark) or  # across the bottom
-            (board[1] == mark and board[4] == mark and board[7] == mark) or  # down the middle
-            (board[2] == mark and board[5] == mark and board[8] == mark) or  # down the middle
-            (board[3] == mark and board[6] == mark and board[9] == mark) or  # down the right side
-            (board[1] == mark and board[5] == mark and board[9] == mark) or  # diagonal
-            (board[3] == mark and board[5] == mark and board[7] == mark))  # diagonal
-import random
+    out_comes_row = [[0, 0, 0], [1, 1, 1], [2, 2, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2]]
+    out_comes_col = [[0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 0, 0], [1, 1, 1], [2, 2, 2], [0, 1, 2], [2, 1, 0]]
+    for row, col in zip(out_comes_row, out_comes_col):
+        if board[row[0]][col[0]] == board[row[1]][col[1]] == board[row[2]][col[2]] == mark:
+            return True
+    return False
+
 def choose_first():
     if random.randint(0, 1) == 0:
         return 'Computer'
     else:
         return 'Player'
+
 def space_check(board, position):
-    return board[position] == ' '
+    return board [int(position[0])][int(position[1])] == ' '
+
 def full_board_check(board):
-    for i in range(1, 10):
-        if space_check(board, i):
-            return False
-    return True
+    if(f==[]):
+        return True
+    else:
+        return False
+
 def player_choice(board):
-    p1 = 0
-    while p1 not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, p1):
-        p1 = int(input('Choose your next position: (1-9) '))
+    p1 ='0'
+    while p1 not in f or not space_check(board, p1):
+        p1 = input('Choose your next position:')
+    f.remove(p1)
     return p1
+
 def computer_choice(board):
-    p2 = random.randint(1, 9)
+    p2 = f[random.randint(0,len(f)-1)]
     while not space_check(board, p2):
-        p2 = random.randint(1, 9)
+        p2 = f[random.randint(0,len(f)-1)]
+    f.remove(p2)
     return p2
-def replay():
-    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
+b = 1
+d = {}
+e = 'y'
 print('Welcome to Tic Tac Toe!')
-while True:
-    # Reset the board
-    theBoard = [' '] * 10
+while b <= 3:
+    a = []
+    f=['00', '01', '02', '10', '11', '12', '20', '21', '22']
+    theBoard = np.array([[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]])
     player_marker, computer_marker = player_input()
+    a.append(player_marker)
+    a.append(computer_marker)
     turn = choose_first()
+    a.append(turn)
     print(turn + ' will go first.')
     play_game = input('Are you ready to play? Enter Yes or No.')
     if play_game.lower()[0] == 'y':
@@ -68,37 +79,64 @@ while True:
         game_on = False
     while game_on:
         if turn == 'Player':
-            # Player's turn.
             display_board(theBoard)
             p1 = player_choice(theBoard)
+            a.append(p1)
             place_marker(theBoard, player_marker, p1)
-
             if win_check(theBoard, player_marker):
                 display_board(theBoard)
                 print('Congratulations! You have won the game!')
+                a.append('Player won')
                 game_on = False
             else:
                 if full_board_check(theBoard):
                     display_board(theBoard)
                     print('The game is a draw!')
+                    a.append('It is a tie')
                     break
                 else:
                     turn = 'Computer'
         else:
-            # Player2's turn.
             display_board(theBoard)
             p2 = computer_choice(theBoard)
+            a.append(p2)
             place_marker(theBoard, computer_marker, p2)
             if win_check(theBoard, computer_marker):
                 display_board(theBoard)
                 print('Computer has won!')
+                a.append('Computer won')
                 game_on = False
             else:
                 if full_board_check(theBoard):
                     display_board(theBoard)
                     print('The game is a draw!')
+                    a.append('It is a tie')
                     break
                 else:
                     turn = 'Player'
-    if not replay():
-        break
+    d[b] = a
+    b = b + 1
+while e == 'y':
+    try:
+        c = int(input("Enter the game no. for the desired information"))
+        if d[c][2] == 'Player':
+            theBoard = np.array([[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]])
+            for i in range(3, len(d[c])-1, 2):
+                place_marker(theBoard, d[c][0], d[c][i])
+                display_board(theBoard)
+                place_marker(theBoard, d[c][1], (d[c][i + 1]))
+                display_board(theBoard)
+        else:
+            theBoard = np.array([[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]])
+            for i in range(3, len(d[c])-1, 2):
+                place_marker(theBoard, d[c][1], d[c][i])
+                display_board(theBoard)
+                place_marker(theBoard, d[c][0], d[c][i + 1])
+                display_board(theBoard)
+        print(d[c][-1])
+    except KeyError:
+        print("Value should be of given range (1-10)")
+    except ValueError:
+        print("Value should be of correct type (integer)")
+    finally:
+        e = input("Would you like to try again? Press y for yes and any other key for no")
