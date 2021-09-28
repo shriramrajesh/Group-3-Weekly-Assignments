a7702f92c67777db5bc27d7c1f8adacc3fd81211
Shriram <90387381+shriramrajesh@users.noreply.github.com>
2021-09-28 13:40:40 +0530

    Update Week 4- Database #2 Question 1.py
    Question 1
    
    Create a Table Employee which will have the four columns, i.e., Name, ID, salary, and Department_id.
    
    Add a new column ‘City’ to the Table Employee.
    
    Insert 5 records into this table.
    
    Read the Name, ID, and Salary from the Employee table and print it.
    
    Print the details of employees whose names start with ‘j’ (or any letter input by the user)
    
    Print the details of employees with ID’s inputted by the user.
    
    Change the name of the employee whose ID is input by the user.

--- Week 4- Database #2 Question 1.py
+++ Week 4- Database #2 Question 1.py
@@ -2,36 +2,26 @@ import sqlite3
 conn = sqlite3.connect('employee.db')
 cursor = conn.cursor()
 cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
-query = """CREATE TABLE EMPLOYEE(
-        Name CHAR(20), 
-        ID INT,
-        Salary INT,
-        Department_id INT)"""
+query = "CREATE TABLE EMPLOYEE(Name CHAR(20),ID INT,Salary INT,Department_id INT)"
 cursor.execute(query)
-conn.execute("ALTER TABLE EMPLOYEE "
-             "ADD City CHAR(20)")
-conn.execute("INSERT INTO EMPLOYEE (Name,ID,Salary,Department_id,City)"
-                 "VALUES ('David',101,40000,1,'Mumbai')")
-conn.execute("INSERT INTO EMPLOYEE (Name,ID,Salary,Department_id,City)"
-                 "VALUES ('Michael',102,20000,2,'New Delhi')")
-conn.execute("INSERT INTO EMPLOYEE (Name,ID,Salary,Department_id,City)"
-                 "VALUES ('Susan',103,25000,3,'Bangalore')")
-conn.execute("INSERT INTO EMPLOYEE (Name,ID,Salary,Department_id,City)"
-                 "VALUES ('James',104,28000,4,'Hyderabdad')")
-conn.execute("INSERT INTO EMPLOYEE (Name,ID,Salary,Department_id,City)"
-                 "VALUES ('Linda',105,42000,5,'Chennai')")
+conn.execute("ALTER TABLE EMPLOYEE ADD City CHAR(20)")
+conn.execute("INSERT INTO EMPLOYEE (Name,ID,Salary,Department_id,City) VALUES ('David',101,40000,1,'Mumbai'),('Michael',102,20000,2,'New Delhi'),('Susan',103,25000,3,'Bangalore'),('James',104,28000,4,'Hyderabdad'),('Linda',105,42000,5,'Chennai')")
 print(conn.execute("SELECT * from EMPLOYEE").fetchall())
 print(conn.execute("SELECT Name,ID,Salary from EMPLOYEE").fetchall())
 a='y'
 while(a=='y'):
-    n=input("Enter the first letter of employee name")
-    print(conn.execute("SELECT * from EMPLOYEE where upper(Name) LIKE'"+ n +"%'").fetchall())
-    id=input("Enter id of employee")
-    print(conn.execute("SELECT * from EMPLOYEE where ID='"+ id +"'").fetchall())
-    id=input("Enter id of employee to change employee name")
-    nn=input("Enter new employee name")
-    conn.execute("UPDATE EMPLOYEE SET Name= '"+ nn +"'where ID='"+ id +"'")
-    print(conn.execute("SELECT * from EMPLOYEE").fetchall())
-    a=print("Would you like to try again? Press y for yes and any other key for no")
+    b=int(input("Enter choice: 1. First Letter 2. ID 3. Update"))
+    if(b==1):
+        n=input("Enter the first letter of employee name")
+        print(conn.execute("SELECT * from EMPLOYEE where upper(Name) LIKE'"+ n +"%'").fetchall())
+    elif(b==2):
+        id=input("Enter id of employee")
+        print(conn.execute("SELECT * from EMPLOYEE where ID='"+ id +"'").fetchall())
+    else:
+        id=input("Enter id of employee to change employee name")
+        nn=input("Enter new employee name")
+        conn.execute("UPDATE EMPLOYEE SET Name= '"+ nn +"'where ID='"+ id +"'")
+        print(conn.execute("SELECT * from EMPLOYEE").fetchall())
+    a=input("Would you like to try again? Press y for yes and any other key for no")
 conn.commit()
 conn.close()
