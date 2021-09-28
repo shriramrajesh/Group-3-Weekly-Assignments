7d1cd7e6649b5b3c38bb5b16b5b1bab957ede42d
Shriram <90387381+shriramrajesh@users.noreply.github.com>
2021-09-28 14:50:01 +0530

    Update Week 4- Database #1 Question 1.py
    Question 1
    
    Write Python code to perform the following:
    
    Create a Table called Cars with two columns - Name of a car, Name of its owner.
    
    Add 10 records into the Cars table. Avoid SQL Injection.
    
    Retrieve all the records and print them.

--- Week 4- Database #1 Question 1.py
+++ Week 4- Database #1 Question 1.py
@@ -2,31 +2,8 @@ import sqlite3
 conn = sqlite3.connect('carowner.db')
 cursor = conn.cursor()
 cursor.execute("DROP TABLE IF EXISTS Cars")
-query = """CREATE TABLE Cars(
-        CARNAME CHAR(20), 
-        OWNERNAME CHAR(20))"""
+query = "CREATE TABLE Cars(CARNAME CHAR(20), OWNERNAME CHAR(20))"
 cursor.execute(query)
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Audi', 'John')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Tata', 'Adams')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Mahindra', ' Baker')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Honda', 'Clark')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Maruti Suzuki', 'Davis')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Toyota', 'Evans')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Chevorlet', 'Frank')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Mitsubishi', 'Ghosh')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('Ford', 'Hills')")
-conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) "
-                 "VALUES ('BMW', 'Irwin')")
-conn.commit()
-cursor = conn.execute("SELECT * from CAROWNER")
-print(cursor.fetchall())
+conn.execute("INSERT INTO Cars (CARNAME,OWNERNAME) VALUES ('Audi', 'John'),('Tata', 'Adams'),('Mahindra', ' Baker'),('Honda', 'Clark'),('Maruti Suzuki', 'Davis'),('Toyota', 'Evans'),('Chevorlet', 'Frank'),('Mitsubishi', 'Ghosh'),('Ford', 'Hills'),('BMW', 'Irwin')")
+print(conn.execute("SELECT * from CAROWNER").fetchall())
 conn.close()
