2e31d880118cc787589404fb5617c37d3f7b51ef
Shriram <90387381+shriramrajesh@users.noreply.github.com>
2021-09-30 18:15:22 +0530

    Create Week 4- Database #3 Question 1.py
    Question 1
    
    Analyse the tables in the database and draw the ER diagram.
    
    Print the names of both the Home Teams and Away Teams in each match played in 2015 and Full-time Home Goals (FTHG) = 5
    
    Print the details of the matches where Arsenal is the Home Team and Full-Time Result (FTR) is “A” (Away Win)
    
    Print all the matches from the 2012 season till the 2015 season where Away Team is Bayern Munich and Full time Away Goals (FTHG) > 2
    
    Print all the matches where the Home Team name begins with “A” and Away Team name starts with “M.”

---
+++ Week 4- Database #3 Question 1.py
@@ -0,0 +1,7 @@
+import sqlite3
+conn = sqlite3.connect('database.sqlite')
+cursor = conn.cursor()                                                                                                              
+print(conn.execute("select HomeTeam,AwayTeam from Matches where Season=2015 and FTHG=5").fetchall())                                
+print(conn.execute("select * from Matches where HomeTeam= 'Arsenal' and FTR='A'").fetchall())                                       
+print(conn.execute("select * from Matches where AwayTeam= 'Bayern Munich' and Season>2012 and Season<2015 and FTHG>2").fetchall())  
+print(conn.execute("select * from Matches where upper(HomeTeam) LIKE 'A%' and upper(AwayTeam) LIKE 'M%'").fetchall())
