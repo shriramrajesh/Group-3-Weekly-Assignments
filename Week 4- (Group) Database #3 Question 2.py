752536df90258663c562f241a2864ba6f274a09b
Shriram <90387381+shriramrajesh@users.noreply.github.com>
2021-09-30 21:04:56 +0530

    Create Week 4- (Group) Database #3 Question 2.py
    Question 2
    
    Counts all the rows in the Teams table
    
    Print all the unique values that are included in the Season column in the Teams table
    
    Print the largest and smallest stadium capacity that is included in the Teams table
    
    Print the sum of squad players for all teams during the 2014 season from the Teams table [Answer - 1164]
    
    Query the Matches table to know how many goals did Man United score during home games on average? [Answer - 2.16]

---
+++ Week 4- (Group) Database #3 Question 2.py
@@ -0,0 +1,8 @@
+import sqlite3
+conn = sqlite3.connect('database.sqlite')
+cursor = conn.cursor() 
+print(conn.execute("select count(*) from Teams").fetchall())                                                                         
+print(conn.execute("select distinct Season from Teams").fetchall())                                                                 
+print(conn.execute("select min(StadiumCapacity),max(StadiumCapacity) from Teams").fetchall())                                        
+print(conn.execute("select sum(KaderHome) from Teams where Season= 2014").fetchall())                                                 
+print(conn.execute("select round(avg(FTHG),2) from Matches where HomeTeam='Man United'").fetchall())
