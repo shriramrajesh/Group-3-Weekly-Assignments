46c73afbf9eab18c56b0968e283825e6b4182453
Shriram <90387381+shriramrajesh@users.noreply.github.com>
2021-12-02 17:56:37 +0530

    Create 3.java

---
+++ Java Training Assignments/3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class Assignment {
+	public static void main(String[] args) 
+	  throws IOException
+	  {
+	   Scanner sc = new Scanner( System.in );
+	   double i;
+	   System.out.println("Input");
+	   i=sc.nextDouble();
+	   System.out.println("Output");
+	   System.out.println("DOUBLE"+i);
+	}
+}
